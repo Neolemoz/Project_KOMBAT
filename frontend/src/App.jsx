7699eab5e032b2a15abad3d4d1335992a3e7c2be
lemon <cielismxbeta@gmail.com>
2026-02-22 03:29:18 +0700

    feat(frontend): init React app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>KOMBAT Frontend is running ✅</h1>
+    </div>
+  )
+}
